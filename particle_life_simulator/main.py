a4a8d49f365ff5a16427d5eed975ad757b7f9215
AAPPHH <jfhamelmann@gmail.com>
2025-02-05 13:13:22 +0100

    Benchmark

--- particle_life_simulator/main.py
+++ particle_life_simulator/main.py
@@ -15,7 +15,7 @@ def main():
     gui = GUI(window_width=screen_width, window_height=screen_height, particle_size=3)
 
     particle_creator = CreateParticle(
-    num_particles=100,
+    num_particles=1000,
     x_max=1920,
     y_max=1080,
     speed_range=(-1, 1),
